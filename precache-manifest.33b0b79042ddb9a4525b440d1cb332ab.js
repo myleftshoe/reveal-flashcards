5f6308d0096b8c4a1160eb830df901e9925e733b
Paul <paulesammut@gmail.com>
2018-12-13 18:02:27 +1100

    Updates

---
+++ precache-manifest.33b0b79042ddb9a4525b440d1cb332ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "537402e0ac15af19a87c",
+    "url": "/reveal-flashcards/static/js/runtime~main.537402e0.js"
+  },
+  {
+    "revision": "c62b3073ae0d2ea3bbad",
+    "url": "/reveal-flashcards/static/js/main.c62b3073.chunk.js"
+  },
+  {
+    "revision": "38968b2308ad9f889eae",
+    "url": "/reveal-flashcards/static/js/1.38968b23.chunk.js"
+  },
+  {
+    "revision": "c62b3073ae0d2ea3bbad",
+    "url": "/reveal-flashcards/static/css/main.0bc26e2a.chunk.css"
+  },
+  {
+    "revision": "f7ae80e1d93d4b050c1acc1213b8e003",
+    "url": "/reveal-flashcards/index.html"
+  }
+];
