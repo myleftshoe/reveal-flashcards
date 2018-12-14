9202546e4c9afc8430c7e5e5e9c001eba91bf6e4
Paul <paulesammut@gmail.com>
2018-12-15 10:25:38 +1100

    Updates

--- precache-manifest.15ef620c54c1e856fb93f8443a107b85.js
+++ precache-manifest.15ef620c54c1e856fb93f8443a107b85.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/reveal-flashcards/static/js/runtime~main.537402e0.js"
   },
   {
-    "revision": "ba0885d7c7978531563e",
-    "url": "/reveal-flashcards/static/js/main.ba0885d7.chunk.js"
+    "revision": "e8f68c937f4fa0ea811a",
+    "url": "/reveal-flashcards/static/js/main.e8f68c93.chunk.js"
   },
   {
     "revision": "ee68cc3cf208144f9b6e",
     "url": "/reveal-flashcards/static/js/1.ee68cc3c.chunk.js"
   },
   {
-    "revision": "ba0885d7c7978531563e",
+    "revision": "e8f68c937f4fa0ea811a",
     "url": "/reveal-flashcards/static/css/main.2462b581.chunk.css"
   },
   {
-    "revision": "bda2e6b17c4ab4ea7f1a54bec86f60cb",
+    "revision": "b4c611874927663475141e12708a0aa7",
     "url": "/reveal-flashcards/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reveal-flashcards/precache-manifest.7b1c225f340d045ff1a45fa511f17fd5.js"
+  "/reveal-flashcards/precache-manifest.15ef620c54c1e856fb93f8443a107b85.js"
 );
 
 workbox.clientsClaim();
