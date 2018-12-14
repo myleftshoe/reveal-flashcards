589ca702769aae8eb3fc1a3ad81d60fc7d39f433
Paul <paulesammut@gmail.com>
2018-12-14 15:32:02 +1100

    Updates

--- precache-manifest.74d53565b2130ef6b9e3608b19fc121f.js
+++ precache-manifest.74d53565b2130ef6b9e3608b19fc121f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/reveal-flashcards/static/js/runtime~main.537402e0.js"
   },
   {
-    "revision": "d23c31cd2e638fb2d48b",
-    "url": "/reveal-flashcards/static/js/main.d23c31cd.chunk.js"
+    "revision": "15211f4e98ea187f4346",
+    "url": "/reveal-flashcards/static/js/main.15211f4e.chunk.js"
   },
   {
     "revision": "12d523717bc5d42331cd",
     "url": "/reveal-flashcards/static/js/1.12d52371.chunk.js"
   },
   {
-    "revision": "d23c31cd2e638fb2d48b",
+    "revision": "15211f4e98ea187f4346",
     "url": "/reveal-flashcards/static/css/main.a83f9dee.chunk.css"
   },
   {
-    "revision": "b81cee50605d7a1308e976a497d1947f",
+    "revision": "c756a4e4a1f15fa4e405425f4ab2d079",
     "url": "/reveal-flashcards/index.html"
   }
 ];
