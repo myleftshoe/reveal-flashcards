502ceec166210a51826b29fe4b9b3b839b13dcf0
Paul <paulesammut@gmail.com>
2018-12-16 17:26:33 +1100

    Updates

---
+++ precache-manifest.aa450d5759333b9128fac7c969cefd2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "537402e0ac15af19a87c",
+    "url": "/reveal-flashcards/static/js/runtime~main.537402e0.js"
+  },
+  {
+    "revision": "bc5adaad23e5b45e330f",
+    "url": "/reveal-flashcards/static/js/main.bc5adaad.chunk.js"
+  },
+  {
+    "revision": "97f8bd2ffc94da5469ff",
+    "url": "/reveal-flashcards/static/js/1.97f8bd2f.chunk.js"
+  },
+  {
+    "revision": "bc5adaad23e5b45e330f",
+    "url": "/reveal-flashcards/static/css/main.ca4263b3.chunk.css"
+  },
+  {
+    "revision": "a30771b1ce30f62ead2f951f0e4654ee",
+    "url": "/reveal-flashcards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reveal-flashcards/precache-manifest.10978b064425608f63865650679ddf5c.js"
+  "/reveal-flashcards/precache-manifest.aa450d5759333b9128fac7c969cefd2f.js"
 );
 
 workbox.clientsClaim();
