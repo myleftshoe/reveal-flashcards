e74163bc08a438eabbecc2178ffb6b54c4e46cea
Paul <paulesammut@gmail.com>
2018-12-19 18:34:22 +1100

    Updates

---
+++ precache-manifest.ba43630589870f4bc2e81ca0fa54f45f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfe9ca569def48b5840d",
+    "url": "/reveal-flashcards/static/js/runtime~main.dfe9ca56.js"
+  },
+  {
+    "revision": "df7fb2948e884f674521",
+    "url": "/reveal-flashcards/static/js/main.df7fb294.chunk.js"
+  },
+  {
+    "revision": "dbc639ff96542cc663f3",
+    "url": "/reveal-flashcards/static/js/6.dbc639ff.chunk.js"
+  },
+  {
+    "revision": "64159eeed8e3c2dbae87",
+    "url": "/reveal-flashcards/static/js/4.64159eee.chunk.js"
+  },
+  {
+    "revision": "cfb3e7238f708b3dd3aa",
+    "url": "/reveal-flashcards/static/js/3.cfb3e723.chunk.js"
+  },
+  {
+    "revision": "5e308042d51171b8b6c4",
+    "url": "/reveal-flashcards/static/js/2.5e308042.chunk.js"
+  },
+  {
+    "revision": "f6f04e92decf7288cd47",
+    "url": "/reveal-flashcards/static/js/1.f6f04e92.chunk.js"
+  },
+  {
+    "revision": "147fa139e4e9d13406fa",
+    "url": "/reveal-flashcards/static/js/0.147fa139.chunk.js"
+  },
+  {
+    "revision": "df7fb2948e884f674521",
+    "url": "/reveal-flashcards/static/css/main.7a549369.chunk.css"
+  },
+  {
+    "revision": "b34618e2a65529d03c14cd9d70340b84",
+    "url": "/reveal-flashcards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reveal-flashcards/precache-manifest.09388af611341f2c30deb2e2b9242f2f.js"
+  "/reveal-flashcards/precache-manifest.ba43630589870f4bc2e81ca0fa54f45f.js"
 );
 
 workbox.clientsClaim();
