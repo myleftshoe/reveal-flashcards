a8abe4edaaf22f178f77e025e8496f7691f6c2e7
Paul <paulesammut@gmail.com>
2018-12-25 16:53:54 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reveal-flashcards/precache-manifest.adc6ba7cf5856b8362a203efc9e0739c.js"
+  "/reveal-flashcards/precache-manifest.52b9d363ca8a2818fc28b9ac4fc7ac51.js"
 );
 
 workbox.clientsClaim();
---
+++ static/js/6.e1b4f76e.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{93:function(n,w){}}]);
+//# sourceMappingURL=6.e1b4f76e.chunk.js.map
